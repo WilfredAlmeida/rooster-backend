3cf6232c117d1b394714cc44e20c3bd0ffd90116
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-08-28 01:22:42 +0530

    feat: initial repo setup
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

---
+++ src/interfaces/collection.ts
@@ -0,0 +1,7 @@
+export interface Collection {
+  nftCount: number;
+  mintAllTo: string | null;
+  collectionName: string;
+  collectionSymbol: string;
+  collectionImage: string | null;
+}
---
+++ src/routes/webhook.ts
@@ -0,0 +1,12 @@
+import {Request, Response, Router} from "express";
+
+const router = Router();
+
+router.post("/", async(req: Request, res: Response)=>{
+
+    
+
+})
+
+
+export default router;
